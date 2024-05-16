be7e5a55279da505142d3d294ab1920abf8092e2
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-16 17:28:10 +0600

    Create 02. Write a code to sum up numbers in a range.c

---
+++ 02. Write a code to sum up numbers in a range.c
@@ -0,0 +1,10 @@
+/*
+(For example: from 1 to 10)
+*/
+#include <stdio.h>
+int main() {
+int a,sum=0;
+for (a =2; a<=10; a+=2) {
+sum += a; }
+printf("The sum of the even numbers from 1 to 10 is: %d \n", sum);
+return 0; }
